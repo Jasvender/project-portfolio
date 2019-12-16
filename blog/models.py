cf75cea6f38dde39604be44c33684ffb9bba51c3
Jasvender <jasvenders@gmail.com>
2019-12-16 09:45:25 +0530

    Jobs and Blog Model is created

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
+# Create a Blog models
+#   Title
+#   public date
+#   Blog Text
+#   Image
+
+# Add Blog App to settings
+
+# Create Migration
+
+# Migrate
+
+# Add to admin
+
 class Blog(models.Model):
 
-    image = models.ImageField(upload_to = 'images/blogs')
-    summery = models.CharField(max_length = 200)
+    image   = models.ImageField(upload_to = 'images/blogs')
+    date    = models.DateTimeField(default='date',null=True,blank=True)
+    title   = models.CharField(max_length = 235, default='')
+    summery = models.TextField()
