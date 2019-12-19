cf97f7edbe266cabb54398468066a1cf2b493241
Jasvender <jasvenders@gmail.com>
2019-12-19 22:45:20 +0530

    Blog Functionality

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.utils import timezone
+# from django.utils import timezone
 
 # Create a Blog models
 #   Title
@@ -22,3 +22,15 @@ class Blog(models.Model):
     pub_date    = models.DateTimeField(blank=True, null=True)
     title   = models.CharField(max_length = 235, default='')
     summery = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def body(self):
+        return self.summery[:100]
+
+    def pub_date_pretty(self):
+        if not self.pub_date:
+            return "None"
+        else:
+            return self.pub_date.strftime('%b %e %Y')
